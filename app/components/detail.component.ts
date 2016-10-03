0b13dc39b00af0eecce303a909325269f140e3dc
hollannikas <hollannikas@users.noreply.github.com>
2016-10-03 18:49:52 +0300

    Removed router, added largest prime factor

--- app/components/detail.component.ts
+++
@@ -1,5 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({selector: 'detail', template: '<h1>Details</h1>'})
-export class Detail {
-}
--- app/index_common.ts
+++ app/index_common.ts
@@ -1,23 +1,79 @@
 import {Component,NgModule} from '@angular/core';
-import {WORKER_APP_LOCATION_PROVIDERS, WorkerAppModule} from '@angular/platform-webworker';
-import {RouterModule} from '@angular/router';
-import {Start} from './components/start.component';
-import {Detail} from './components/detail.component';
+import {WorkerAppModule} from '@angular/platform-webworker';
 
-@Component({selector: 'app', templateUrl: 'app.component.html'})
+var NUM_ITEMS_PER_ROUND: number = 30;
+const MAX_ITEMS = 100000;
+
+@Component({
+  selector: 'app', 
+  templateUrl: 'app.component.html',
+  host: {
+    '(window:scroll)': 'createData($event)'
+    }
+  })
 export class App {  
+
+  lastNum = Math.pow(2, 25);
+  items: Array<DataRow>  = [];
+
+  constructor() {
+    this.createData();
+  }
+
+  public createData(): void {
+    console.log("Generating data");
+    if (this.items.length > MAX_ITEMS) {
+      return;
+    }
+    for (var i = 0; i < NUM_ITEMS_PER_ROUND; i++){
+      var num = this.lastNum * 1.05;
+      var factor = this._findLargestPrimeFactor(num);
+      this.items.push({
+        num: num,
+        factor: factor
+      });
+
+      this.lastNum = num;
+    }
+    console.log("Generated data");
+  }
+
+  private _findFactors(num: number): Array<number> {
+    var factors = [];
+    for (var i = 1; i < Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        factors.push(i);
+      }
+    }
+    return factors;
+  }
+
+  private _isPrime(num: number): boolean {
+    return this._findFactors(num).length === 1;
+  }
+
+  private _findLargestPrimeFactor(num: number): number {
+    var factors = this._findFactors(num);
+    if (factors.length === 0) {
+      // number is prime
+      return num
+    }
+    var primeFactors = factors.filter((num: number) => this._isPrime(num));
+    return primeFactors[primeFactors.length - 1];
+  }
 }
 
-export const ROUTES = [
-  {path: '', component: Start}, 
-  {path: 'detail', component: Detail}
-];
+class DataRow {
+  num: number;
+  factor: number;
+}
+
 
 @NgModule({
-  imports: [WorkerAppModule, RouterModule.forRoot(ROUTES, {useHash: true})],
-  providers: [WORKER_APP_LOCATION_PROVIDERS],
+  imports: [WorkerAppModule],
+  providers: [],
   bootstrap: [App],
-  declarations: [App, Start, Contact, About]
+  declarations: [App]
 })
 export class AppModule {
 }
